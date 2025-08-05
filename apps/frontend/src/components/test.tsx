ddbac834f0db9e2cd11d312af687cc4c61e16923
Amama-Fatima <amamafatima58@gmail.com>
2025-08-05 18:57:25 +0500

    get session test component

---
+++ apps/frontend/src/components/test.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { createSupabaseBrowserClient } from "@/supabase/browser-client";
+import React, { useEffect } from "react";
+
+const Test = () => {
+  const supabaseBrowserClient = createSupabaseBrowserClient();
+  const [session, setSession] = React.useState(null);
+  useEffect(() => {
+    async function getSession() {
+      try {
+        const { data: session, error } =
+          await supabaseBrowserClient.auth.getSession();
+        console.log("Session:", session);
+        setSession(session as any);
+      } catch (error) {
+        console.error("Error fetching session:", error);
+      }
+    }
+    getSession();
+  }, []);
+  return <div>Test: {JSON.stringify(session)}</div>;
+};
+
+export default Test;
