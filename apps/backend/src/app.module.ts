86af36e4837a0f69135c95dd077b17837a2f51a7
Amama-Fatima <amamafatima58@gmail.com>
2025-08-13 02:10:03 +0500

    deleted chapters and folders directories in the backend. will be changing the db significantly

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { ChaptersModule } from "./chapters/chapters.module";
 import { SupabaseModule } from "./supabase/supabase.module";
 import { ConfigModule } from "@nestjs/config";
-import { FoldersModule } from "./folders/folders.module";
 
 @Module({
   controllers: [AppController],
@@ -15,8 +13,6 @@ import { FoldersModule } from "./folders/folders.module";
       envFilePath: ".env",
     }),
     SupabaseModule,
-    ChaptersModule,
-    FoldersModule,
   ],
 })
 export class AppModule {}
