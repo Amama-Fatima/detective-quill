8cff9b8b647fed0a1bff4b9f074d6eb9e96dfe23
Amama-Fatima <amamafatima58@gmail.com>
2025-08-28 01:28:49 +0500

    project workspace page. only ui. no backend functionality

---
+++ apps/frontend/src/app/workspace/[projectId]/page.tsx
@@ -0,0 +1,42 @@
+import ProjectWorkspaceHeader from "@/components/project-workspace/project-workspace-header";
+import WorkspaceMainBody from "@/components/project-workspace/project-workspace-main";
+import { createSupabaseServerClient } from "@/supabase/server-client";
+import { redirect } from "next/navigation";
+import React from "react";
+interface ProjectWorkspacePageProps {
+  params: Promise<{
+    projectId: string;
+  }>;
+}
+
+const ProjectWorkspace = async ({ params }: ProjectWorkspacePageProps) => {
+  const supabase = await createSupabaseServerClient();
+
+  const { projectId } = await params;
+
+  const {
+    data: { user },
+    error: authError,
+  } = await supabase.auth.getUser();
+
+  // Redirect to sign-in if not authenticated
+  if (authError || !user) {
+    redirect("/auth/sign-in");
+  }
+
+  const { data, error } = await supabase
+    .from("projects")
+    .select("*")
+    .eq("id", projectId)
+    .eq("author_id", user.id)
+    .single();
+
+  return (
+    <div>
+      <ProjectWorkspaceHeader project={data} />
+      <WorkspaceMainBody project={data} />
+    </div>
+  );
+};
+
+export default ProjectWorkspace;
