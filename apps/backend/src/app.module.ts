b58ab89d5c52da0250e96480c428cd98aeb47fa6
Amama-Fatima <amamafatima58@gmail.com>
2025-09-05 23:03:23 +0500

    some additions to the comments folder in the backend

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -10,6 +10,7 @@ import { BlueprintsModule } from "./blueprints/blueprints.module";
 import { CardTypesModule } from "./card_types/card_types.module";
 import { BlueprintCardsModule } from "./blueprint_cards/blueprint_cards.module";
 import { ClientsModule, Transport } from "@nestjs/microservices";
+import { CommentsModule } from "./comments/comments.module";
 
 @Module({
   controllers: [AppController],
@@ -38,6 +39,7 @@ import { ClientsModule, Transport } from "@nestjs/microservices";
     BlueprintsModule,
     CardTypesModule,
     BlueprintCardsModule,
+    CommentsModule,
   ],
 })
 export class AppModule {}
