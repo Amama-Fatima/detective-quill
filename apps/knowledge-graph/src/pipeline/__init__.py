e71dda7e0b56a6d0578c8e4b3095a28ca0cc12ae
Amama-Fatima <amamafatima58@gmail.com>
2026-02-07 01:23:29 +0500

    layer 2. post process the results of layer 1

--- apps/knowledge-graph/src/pipeline/__init__.py
+++ apps/knowledge-graph/src/pipeline/__init__.py
@@ -1,5 +1,7 @@
 from src.pipeline.layer1_spacy import extract_entities_layer1
+from src.pipeline.layer2_postprocess import postprocess_entities_layer2
 
 __all__ = [
     "extract_entities_layer1",
+    "postprocess_entities_layer2",
 ]
