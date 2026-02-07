b9321c140202f30d7b587ab965a29faefb5fc30c
Amama-Fatima <amamafatima58@gmail.com>
2026-02-07 16:11:17 +0500

    creating the full pipeline using all the 4 layers

--- apps/knowledge-graph/src/pipeline/__init__.py
+++ apps/knowledge-graph/src/pipeline/__init__.py
@@ -1,9 +1,14 @@
 from src.pipeline.layer1_spacy import extract_entities_layer1
 from src.pipeline.layer2_postprocess import postprocess_entities_layer2
 from src.pipeline.layer3_enrichment import enrich_entities_layer3
+from src.pipeline.layer4_relationships import extract_relationships_layer4
+from src.pipeline.orchestrator import NarrativeAnalysisPipeline, process_scene
 
 __all__ = [
     "extract_entities_layer1",
     "postprocess_entities_layer2",
     "enrich_entities_layer3",
+    "extract_relationships_layer4",
+    "NarrativeAnalysisPipeline",
+    "process_scene",
 ]
