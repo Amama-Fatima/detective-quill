161da5dfb6a31abc7b03a85b7c445d42fef5a222
Amama-Fatima <amamafatima58@gmail.com>
2026-02-11 01:35:55 +0500

    separated nlp analysis service for worker. The worker functions that need to talk to supabase now use the admin supabase service to bypass rls

--- apps/backend/worker/worker.module.ts
+++ apps/backend/worker/worker.module.ts
@@ -1,12 +1,14 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { EmailConsumer } from "./email.consumer";
-import { EmailModule } from "../src/email/email.module";
 import { NlpAnalysisConsumer } from "./nlp-analysis.consumer";
-import { NlpAnalysisModule } from "src/nlp-analysis/nlp-analysis.module";
+import { EmailModule } from "../src/email/email.module";
+import { AdminSupabaseService } from "../src/supabase/admin-supabase.service";
+import { WorkerNlpAnalysisService } from "../src/nlp-analysis/worker-nlp-analysis.service";
 
 @Module({
-  imports: [ConfigModule, EmailModule, NlpAnalysisModule],
+  imports: [ConfigModule.forRoot(), EmailModule],
   controllers: [EmailConsumer, NlpAnalysisConsumer],
+  providers: [AdminSupabaseService, WorkerNlpAnalysisService],
 })
 export class WorkerModule {}
