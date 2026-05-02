17bb26ef46574786a83f03dbd528d059fdf86915
NoorFatima01 <nfatima101204@gmail.com>
2026-05-02 23:58:33 +0500

    added example envs

--- apps/knowledge-graph/src/config.py
+++ apps/knowledge-graph/src/config.py
@@ -6,11 +6,6 @@ load_dotenv()
 
 
 class Settings:
-    # RABBITMQ_HOST: str = os.environ.get("RABBITMQ_HOST", "localhost")
-    # RABBITMQ_PORT: int = int(os.environ.get("RABBITMQ_PORT", "5672"))
-    # RABBITMQ_USER: str = os.environ.get("RABBITMQ_USER", "guest")
-    # RABBITMQ_PASSWORD: str = os.environ.get("RABBITMQ_PASSWORD", "guest")
-    # RABBITMQ_VHOST: str = os.environ.get("RABBITMQ_VHOST", "/")
     RABBITMQ_HEARTBEAT: int = int(os.environ.get("RABBITMQ_HEARTBEAT", "60"))
     RABBITMQ_BLOCKED_CONNECTION_TIMEOUT: int = int(
         os.environ.get("RABBITMQ_BLOCKED_CONNECTION_TIMEOUT", "30")
@@ -22,8 +17,6 @@ class Settings:
         "CLOUDAMQP_URL", os.environ.get("RABBITMQ_URL", None)
     )
 
-    RABBITMQ_PREFETCH_COUNT: int = int(os.environ.get("RABBITMQ_PREFETCH_COUNT", "1"))
-
     QUEUE_POLL_INTERVAL_SECONDS: int = max(
         1, int(os.environ.get("QUEUE_POLL_INTERVAL_SECONDS", "120"))
     )
@@ -31,17 +24,14 @@ class Settings:
 
     MODEL_NAME: str = os.environ.get("MODEL_NAME", "teknium/OpenHermes-2.5-Mistral-7B")
     MODEL_DEVICE: str = os.environ.get("MODEL_DEVICE", "cuda")
-    MODEL_MAX_LENGTH: int = int(os.environ.get("MODEL_MAX_LENGTH", "2048"))
-    MODEL_TEMPERATURE: float = float(os.environ.get("MODEL_TEMPERATURE", "0.1"))
+    MODEL_MAX_LENGTH: int = int(os.environ.get("MODEL_MAX_LENGTH", "512"))
+    # MODEL_TEMPERATURE: float = float(os.environ.get("MODEL_TEMPERATURE", "0.1"))
 
     SPACY_MODEL: str = os.environ.get("SPACY_MODEL", "en_core_web_lg")
 
     FILTERED_ENTITY_TYPES: list = os.environ.get(
         "FILTERED_ENTITY_TYPES", "TIME,DATE,CARDINAL,MONEY,PERCENT"
     ).split(",")
-
-    # API_HOST: str = os.environ.get("API_HOST", "0.0.0.0")
-    # API_PORT: int = int(os.environ.get("API_PORT", "8000"))
 
     LOG_LEVEL: str = os.environ.get("LOG_LEVEL", "INFO")
 
